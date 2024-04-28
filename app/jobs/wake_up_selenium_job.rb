89d9b931452eb7cd43a86e26d0c1a6c44f2286c5
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-04-28 13:31:29 -0300

    Refactor WakeUpSelenium job

--- app/jobs/wake_up_selenium_job.rb
+++ app/jobs/wake_up_selenium_job.rb
@@ -4,6 +4,9 @@ class WakeUpSeleniumJob < ApplicationJob
   def perform
     Rails.logger.info 'Walking up Selenium server'
     driver = Driver.new
+  rescue StandardError => e
+    Rails.logger.warn "Error trying to wake up Selenium: #{e.message}"
+  ensure
     driver.quit
     redirect_to root_path
   end
