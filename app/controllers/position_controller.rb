d8d3c7abe92ca22c5e7532e5c1f6af6e3714bf8f
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-04-29 11:17:28 -0300

    Move scraper to job

--- app/controllers/position_controller.rb
+++ app/controllers/position_controller.rb
@@ -7,40 +7,11 @@ ActiveRecord::Base.establish_connection(adapter: 'postgresql', host: uri.host, u
 class PositionController < ApplicationController
   def index
     @positions = Position.all.order('created_at ASC').reverse_order
+    ScraperJob.perform_later
     render json: @positions.to_json, status: :ok
-  end
-
-  def scraper
-    wake_up_selenium
-    delete_old_positions
-    scrape
   end
 
   def test
     render json: { message: 'test' }, status: :ok
-  end
-
-  private
-
-  def delete_old_positions
-    Rails.logger.info "Deleting old positions"
-    Position.where('created_at < ?', 1.month.ago).delete_all
-    Rails.logger.info "Old positions deleted"
-  end
-
-  def scrape
-    Rails.logger.info "Initializing scrapping"
-    Scraper.scrape
-    @status = true
-    render json: @status.to_json
-  end
-
-  def wake_up_selenium
-    Rails.logger.info 'Walking up Selenium server'
-    driver = Driver.new
-  rescue StandardError => e
-    Rails.logger.warn "Error trying to wake up Selenium: #{e.message}"
-  ensure
-    driver.quit
   end
 end
--- app/jobs/scraper_job.rb
+++ app/jobs/scraper_job.rb
@@ -2,9 +2,9 @@ class ScraperJob < ApplicationJob
   queue_as :default
 
   def perform
+    wake_up_selenium
     delete_old_positions
-    Rails.logger.info "Initializing scrapping"
-    Scraper.scrape
+    scrape
   end
 
   private
@@ -13,5 +13,21 @@ class ScraperJob < ApplicationJob
     Rails.logger.info "Deleting old positions"
     Position.where('created_at < ?', 1.month.ago).delete_all
     Rails.logger.info "Old positions deleted"
+  end
+
+  def scrape
+    Rails.logger.info "Initializing scrapping"
+    Scraper.scrape
+    @status = true
+    render json: @status.to_json
+  end
+
+  def wake_up_selenium
+    Rails.logger.info 'Walking up Selenium server'
+    driver = Driver.new
+  rescue StandardError => e
+    Rails.logger.warn "Error trying to wake up Selenium: #{e.message}"
+  ensure
+    driver.quit
   end
 end
