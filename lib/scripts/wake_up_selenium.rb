1f14e021ae9c77f6f0e93478e82a5056b87cd0bd
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-05-01 20:18:55 -0300

    Refactor WUS with rescue

--- lib/scripts/wake_up_selenium.rb
+++ lib/scripts/wake_up_selenium.rb
@@ -10,15 +10,16 @@ use_ssl = (url.scheme == 'https')
 open_timeout = 120
 read_timeout = 150
 
-response = Net::HTTP.start(host, port, use_ssl:, open_timeout:, read_timeout:) do |http|
-  http.request(request)
-end
-p response
-if response.code == '200'
-  p "Server up"
-else
-  p "Error: #{response.code} - #{response.message}"
-end
+begin
+  response = Net::HTTP.start(host, port, use_ssl: use_ssl, open_timeout: open_timeout, read_timeout: read_timeout) do |http|
+    http.request(request)
+  end
 
-rescue => e
-  p e
+  if response.code == '200'
+    puts "Server up"
+  else
+    puts "Error: #{response.code} - #{response.message}"
+  end
+rescue StandardError => e
+  puts "Error: #{e.message}"
+end
