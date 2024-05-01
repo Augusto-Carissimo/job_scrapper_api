3c3366a76bbc4899b841210125ebd460a7d4a777
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-05-01 10:55:15 -0300

    Refactor WakeUpSeleniumJob

--- app/jobs/wake_up_selenium_job.rb
+++ app/jobs/wake_up_selenium_job.rb
@@ -1,12 +1,27 @@
+require 'net/http'
+require 'uri'
+
 class WakeUpSeleniumJob < ApplicationJob
   queue_as :default
 
+  url = URI.parse('https://standalone-chrome-beta-2.onrender.com/ui/')
+
   def perform
     Rails.logger.info 'Walking up Selenium server'
-    driver = Driver.new
+
+    request = Net::HTTP::Get.new(url)
+    host = url.host
+    port = url.port
+    use_ssl = (url.scheme == 'https')
+    open_timeout = 120
+    read_timeout = 150
+
+    response = Net::HTTP.start(host, port, use_ssl:, open_timeout:, read_timeout:) do |http|
+      http.request(request)
+    end
+
+    Rails.logger.info 'Selenium server active'
   rescue StandardError => e
     Rails.logger.warn "Error trying to wake up Selenium: #{e.message}"
-  ensure
-    driver.quit
   end
 end
