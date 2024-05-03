49bcbac266e3b0d8c586526c6f15d2ed68eb027f
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-05-03 15:41:31 -0300

    Refactor RailsFactory log

--- app/scrapers/rails_factory.rb
+++ app/scrapers/rails_factory.rb
@@ -4,14 +4,14 @@ class RailsFactory < Driver
   SEARCH_KEY = 'https://railsfactory.com/careers'
 
   def search
-    Rails.logger.info 'Starting Rails Carma search'
+    Rails.logger.info 'Starting Rails Factory search'
     @driver.navigate.to SEARCH_KEY
     elements = @driver.find_element(:class, 'career-loop-content')
                       .find_elements(:css, 'article.noo_job.hent')
     assign_values(elements)
     Rails.logger.info 'Search finished'
   rescue StandardError => e
-    Rails.logger.warn e.message
+    Rails.logger.warn "Error at Rails Factory scrapping: #{e.message}"
   ensure
     quit
   end
@@ -21,7 +21,7 @@ class RailsFactory < Driver
   def assign_values(elements)
     elements.each do |element|
       title = element.text.chomp("Apply Now")
-      company = 'Factory Rails'
+      company = 'Rails Factory'
       link = element.find_element(:css, 'a.btn.btn-primary.apply-btn').attribute('href')
       website = 'https://railsfactory.com/'
       Position.create!(title:, company:, link:, website:) if Position.find_by(title:).nil?
